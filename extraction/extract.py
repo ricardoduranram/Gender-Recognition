4287de5827cbd0af7070d87fdd9a34b393421fea
Ricardo Duran <ricardo.duran.ram@gmail.com>
2018-05-31 16:33:20 -0700

    Add files via upload

---
+++ extraction/extract.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import face
+import cv2
+
+dataset = np.load("faces(1,-1).npy").item()
+train_samples = dataset["images_train"]
+train_labels = dataset["labels_train"]
+test_samples = dataset["images_test"]
+test_labels = dataset["labels_test"]
+
+
+landmarks1 = face.extract_landmarks(train_samples)
+features1 = face.extract_features(landmarks1)
+
+
+landmarks2 = face.extract_landmarks(test_samples)
+features2 = face.extract_features(landmarks2)
+
+dataset =(features1,train_labels,features2,test_labels)
+np.save("face_features3",dataset)
+import ipdb; ipdb.set_trace()
+
+
+
